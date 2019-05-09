549e69560478a340c8b3c7f2f686655d48a39352
Chris Hodgson <chris.3.hodgson@plus.net>
2019-05-09 11:18:21 +0100

    change import

--- models/UserChallenge.js
+++ models/UserChallenge.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-const MountainMinimal = require("./MountainMinimal");
 
 const userChallengeSchema = new Schema({
   climbedCount: Number,
   remainingCount: Number,
   _challengeId: { type: Schema.Types.ObjectId, ref: "Challenge" },
   _userId: { type: Schema.Types.ObjectId, ref: "User" },
-  _climbedIds: [ MountainMinimal ]
+  _climbedMountainIds: [{ type: Schema.Types.ObjectId, ref: "Mountain" }]
 });
 
 mongoose.model("userChallenges", userChallengeSchema);
